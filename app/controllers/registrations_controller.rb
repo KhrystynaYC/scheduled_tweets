3bd14699f0fdae57da5ed7884e7034943a28a59d
Khrystyna Leshko <k.leshko@youngcapital.nl>
2022-04-29 16:07:00 +0200

    sign up the user & show error message

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,4 +3,25 @@ class RegistrationsController < ApplicationController
     # Instance variable is available also in views
     @user = User.new
   end
+
+  def create
+    #params
+    # -> {"authenticity_token"=>"bNxY82LbiuLV9FQVKALAw0SA5haF+ttepqr1zQGZ4/fWLNr9nAFcIZyfPOO4H0pVLblmDveY73EeZG5DhZbTwQ==", "user"=>{"email"=>"dadada@da.com", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]"}, "commit"=>"Sign Up"}
+
+    #params[:user]
+    # -> {"email"=>"dadada@da.com", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]"}, "commit"=>"Sign Up"}
+
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Great Successs!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
